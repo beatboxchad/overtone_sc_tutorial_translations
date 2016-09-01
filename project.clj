e7f54d360e65dc0b4ca34a46b811decb43e2d7ed
Chad Cassady <chad.cassady@gmail.com>
2016-09-01 11:26:29 -0700

    added overtone dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject sc_tutorial_translations "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Translating the SC code from Eli Fieldsteel's Youtube tutorial series into Clojure/Overtone."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.1"]
+                 ])
