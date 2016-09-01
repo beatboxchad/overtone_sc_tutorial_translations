f74f880eae957e7e24ec7cde2550290f67761192
Chad Cassady <chad.cassady@gmail.com>
2016-09-01 11:22:18 -0700

    new lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sc_tutorial_translations
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sc_tutorial_translations/core_test.clj
@@ -0,0 +1,7 @@
+(ns sc-tutorial-translations.core-test
+  (:require [clojure.test :refer :all]
+            [sc-tutorial-translations.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
